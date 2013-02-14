3e4457535e542261c7f3b7269e787a306472aaa6
IVAN <ivantrrp@gmail.com>
2013-02-14 11:47:19 -0500

    version final

--- python/frecuencias.py
+++ python/frecuencias.py
@@ -5,18 +5,16 @@ ar= sys.argv[1]
 infile=open(ar)
 text= infile.readlines()
 infile.close()
-
+tama=0.0
 # se crea la biblioteca de cada libro con los caracter
 biblioteca = []
-#text= unicode(text,"utf-8")
-
 for line in text:
     for letter in line:
         biblioteca.append(letter)
 biblioteca = list(set(biblioteca))
 
 # se remueve los caracteres no deseados
-paraquitar = ['\n', ' ']
+paraquitar = ['\n', ' ', '.', ',', '%' , ')']
 for item in paraquitar:
     biblioteca.remove(item)
 
@@ -24,14 +22,17 @@ for item in paraquitar:
 ssize=len(biblioteca)
 frecuencia=range(ssize)
 for i in range(len(frecuencia)):
-    frecuencia[i] =0
+    frecuencia[i] =0.0
+for line in text:
+    for c in line:
+        tama+=1
 for line in text:
     for c in range(ssize):
-        frecuencia[c] += line.count(biblioteca[c])
+        frecuencia[c] += line.count(biblioteca[c])/tama
 
 # se crea un arhivo de texto para guardar los datos de frecuencia  
 grabado=open("frecuencias_"+ar, "w")
 
 for d in range(ssize):
-    grabado.write("%s %d\n"%(biblioteca[d], frecuencia[d]))
+    grabado.write("%s %f\n"%(biblioteca[d], frecuencia[d]))
 grabado.close()
