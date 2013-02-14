2076062931c5713010a64728471a8036d417c566
IVAN <ivantrrp@gmail.com>
2013-02-14 11:17:17 -0500

    version final

--- python/frecuencias.py
+++ python/frecuencias.py
@@ -1,59 +1,45 @@
 
 import sys, string, os
-
 # se carga el archivo 
 ar= sys.argv[1]
 infile=open(ar)
 text= infile.readlines()
+#text=u(text)
 infile.close()
 a=0
 b=0
 c=0
 d=0
 
-# se elimina los espacios y demas simbolos en cada fila del documento
-#tam = len(text)
-#for a in text(tam):
-#    text[a]=text[a].rstrip('\n')
-#    text[a]=text[a].rstrip(' ')
-        
-# convertir en una lista los arreglos de cada fila 
-#for b in range(tam):
-#    if b==0:
-#        texto= list(text[b])
-#    else:
-#        texto+=list(text[b])    
-
-# se crea la biblioteca de cada libro con los caracteres dentro de el
+# se crea la biblioteca de cada libro con los caracter
 biblioteca = []
+#text= unicode(text,"utf-8")
+
 for line in text:
     for letter in line:
         biblioteca.append(letter)
-
 biblioteca = list(set(biblioteca))
 
+# se remueve los caracteres no deseados
 paraquitar = ['\n', ' ']
 for item in paraquitar:
     biblioteca.remove(item)
-print biblioteca
+
 # se calcula la frecuencia de cada caracter
-
-
 ssize=len(biblioteca)
 frecuencia=range(ssize)
-
 for i in range(len(frecuencia)):
     frecuencia[i] =0
-
 for line in text:
-    for c in range(len(biblioteca)):
+    for c in range(ssize):
         frecuencia[c] += line.count(biblioteca[c])
 
-# se crea un arhivo 
+# se crea un arhivo de texto para guardar los datos de frecuencia  
 grabado=open("frecuencias_"+ar, "w")
 
 for d in range(ssize):
     grabado.write("%s %d\n"%(biblioteca[d], frecuencia[d]))
 grabado.close()
+
 
 
